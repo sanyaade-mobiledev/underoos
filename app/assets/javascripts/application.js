d3d0cf81546ea17ad73558447d0e12db81bfcbcd
Matthew Kitt <mk.kitt@gmail.com>
2011-11-08 17:22:11 -0700

    Various cleanup and javascript additions

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,7 @@
+
+//= require vendor/jquery-1.6.4.min
+//= require vendor/bootstrap-scrollspy
+//= require vendor/bootstrap-twipsy
+//= require vendor/bootstrap-popover
+//= require vendor/prettify
+
