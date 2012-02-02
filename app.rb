eeb3565cecaa366083b118879ba5bf8486725d1d
Matthew Kitt <mk.kitt@gmail.com>
2012-02-01 18:06:37 -0700

    Tweaking layout settings for more fluidity

--- app.rb
+++ app.rb
@@ -12,7 +12,7 @@ module App
     before do
       @lorem = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
       @lorem_sm = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.'
-      @lorem_dd = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt.'
+      @lorem_dd = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.'
       @default_message = "The default color, when no flag level is passed"
       @notice_message = "Used for basic messages, generally low priority"
       @important_message = "System updates, changes, something the user needs to know about"
