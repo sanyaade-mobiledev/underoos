3ee18575af5dd2255719f76b3df9a232c9582360
Matthew Kitt <mk.kitt@gmail.com>
2012-02-06 22:29:47 -0700

    Auto render images in public/images/* to the guide

--- app.rb
+++ app.rb
@@ -13,6 +13,7 @@ module App
       @lorem = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
       @lorem_sm = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.'
       @lorem_tiny = 'Lorem ipsum dolor sit amet.'
+      @images = Dir['public/images/**/*.{png,jpeg,jpg,gif}'].map {|f| f.sub('public','')}
       @greys = [
         {prop:'$black', hex:'#000'},
         {prop:'$grey1', hex:'#111'},
