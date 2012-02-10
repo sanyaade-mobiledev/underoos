e9ee743c6ab30c9328170507e2871a6712d19a82
Matthew Kitt <mk.kitt@gmail.com>
2012-02-10 16:35:05 -0700

    Showcase various nav systems via interactive demo

--- app/assets/javascripts/styleguide.js
+++ app/assets/javascripts/styleguide.js
@@ -1,7 +1,12 @@
 
+// This is some dirty link disabling and showcasing behavior
 !function ($) {
 
   $(function() {
+    var showcase = $('#navs_showcase')
+    var nav_lists = $('li', showcase)
+    var nav_links = $('li > a', showcase)
+
     // enable nice looking pre tags
     window.prettyPrint && prettyPrint()
 
@@ -13,6 +18,17 @@
     $('.sg-section [href^=#]').click(function (e) {
       e.preventDefault()
     })
+
+    // Showcase various classes of nav items
+    nav_links.on('click', function(e) {
+      e.preventDefault()
+      var li = $(this).parent('li')
+      var classes = $(this).data('classes')
+      nav_lists.removeClass('active')
+      $(li).addClass('active')
+      showcase.removeClass().addClass(classes)
+    })
+
   })
 
 }(window.jQuery)
